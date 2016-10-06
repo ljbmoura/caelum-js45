c92129601a60ede53cf487d9796501a4dbb23e37
ljbmoura <ljbmoura@gmail.com>
2016-10-06 19:55:36 -0300

    update e save via resource

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,32 +1,72 @@
 angular.module ("alurapic").controller ("FotoController", 
-	function ($scope, $http, $routeParams, $location, $resource) {
+	function ($scope, $routeParams, $location, $resource) {
 
 
 		$scope.foto = {};
 		$scope.mensagem = "";
 
+		// verbo GET para foto
 		/*if ($routeParams.id) {
 			$http.get("/v1/fotos/" + $routeParams.id)
 				.success( function (foto) {
 					$scope.foto = foto;
 				})
 		};*/
-		var recursoFoto = $resource("/v1/fotos/:fotoId");
-		recursoFoto.get (
-			{fotoId: $routeParams.id},
-			function (foto) {
-				$scope.foto = foto;
-				console.log ("get foto via resource");
-			},
-			function (erro) {
-				console.log (erro);
-			}
-		);
+		var recursoFoto = $resource("/v1/fotos/:fotoId",
+			null, // uso opcional querystring
+			{
+				update: {
+					method: "put"
+				}
+			});
 
+		if ($routeParams.id) {
+			recursoFoto.get (
+				{fotoId: $routeParams.id},
+				function (foto) {
+					$scope.foto = foto;
+					console.log ("get foto via resource");
+				},
+				function (erro) {
+					console.log (erro);
+				}
+			)
+		}
 
+ 
 		$scope.submeter = function () {
 			if ($scope.formulario.$valid) {
 				if (! $routeParams.id) {
+					recursoFoto.save (
+						$scope.foto,
+						function () {
+							console.log("foto inserida: " + JSON.stringify($scope.foto));
+							$scope.foto = {};
+							$scope.mensagem = "foto incluída com sucesso";
+						}, 
+						function (erro) {
+							console.log (erro);
+							$scope.mensagem = "nao foi possivel incluir a foto";						
+						}
+					)
+				} else {
+					recursoFoto.update (
+						{fotoId: $scope.foto._id},
+						$scope.foto,
+						function () {
+							console.log("foto alterada: " + JSON.stringify($scope.foto));
+							alert("foto " + $scope.foto.titulo + " alterada com sucesso");
+							$location.path("/");
+							//$scope.mensagem = "foto " + $scope.foto.titulo + " alterada com sucesso";
+						},
+						function (erro) {
+							console.log (erro);
+							$scope.mensagem = "nao foi possivel alterar a foto " + $scope.foto.titulo;
+						}
+					)
+				}
+
+				/*
 					$http.post("/v1/fotos", $scope.foto)
 					.success (function () {
 						console.log("foto inserida: " + JSON.stringify($scope.foto));
@@ -50,12 +90,10 @@ angular.module ("alurapic").controller ("FotoController",
 						console.log (erro);
 						$scope.mensagem = "nao foi possivel alterar a foto";
 					});
-				}
+				}*/
 			}
-			
 			//console.log($scope.foto);
 			//$scope.foto = {};
 		}
-
 	}
 );
